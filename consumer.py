556da22ac78b6373293b25892e6f178bcd093f39
Jaswin <51850086+winatan@users.noreply.github.com>
2022-05-10 16:28:48 +0800

    change the host

--- consumer.py
+++ consumer.py
@@ -20,5 +20,6 @@ def main():
 
     channel.start_consuming()
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
